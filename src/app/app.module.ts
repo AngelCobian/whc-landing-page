84296fbf2bebe5136ea3d6c0881ea56dc00be106
AngelCobian <angelcobian@outlook.com>
2018-07-19 17:00:39 -0700

    building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SocialComponent } from './social/social.component';
+import { MobileAppsComponent } from './mobile-apps/mobile-apps.component';
+import { FooterComponent } from './footer/footer.component';
+import { CampaignComponent } from './campaign/campaign.component';
+import { StartComponent } from './start/start.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialComponent,
+    MobileAppsComponent,
+    FooterComponent,
+    CampaignComponent,
+    StartComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
